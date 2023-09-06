c32eab47d1759b575af51a52fb371cf1c749dc76
YoungdahlE <YoungdahlE@gmail.com>
2023-09-06 11:06:55 +0800

    Connect to login API

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+import evaluate
+
+
+rouge = evaluate.load('rouge')
+
+
+def splitSentence(sentence):
+    sentences = []
+    periods = [m.start() for m in re.finditer('\.\s', sentence)]
+    abbreviations = [m.end() for m in re.finditer('(?:[A-Z][a-zA-Z]{0,}\.){1,}', sentence)]
+    sentence_ends = periods
+    for index in range(len(sentence_ends)):
+        for item in abbreviations:
+            if sentence_ends[index] + 1 == item:
+                sentence_ends[index] = 0
+    if sentence_ends.count(0) > 0:
+        while 0 in sentence_ends: sentence_ends.remove(0)
+    if len(sentence_ends) > 0:
+        sentence_ends.insert(0, -2)
+        sentence_ends.append(len(sentence))
+        for index in range(len(sentence_ends)-1):
+            sentences.append(sentence[sentence_ends[index]+2:sentence_ends[index+1]+1])
+        return sentences
+    else:
+        return [sentence]
+
 
 summarizer = pipeline(
     "summarization",
@@ -27,7 +53,12 @@ async def demo_get():
 
 @app.post("/path")
 async def demo_post(inp: Msg):
-    return {"message": summarizer(inp.msg)}
+    summary = summarizer(inp.msg[:2048])
+    results = rouge.compute(predictions=[splitSentence(inp.msg)], references=[splitSentence(summary)])
+    return {
+        "summary": summary,
+        "rouge": results['rougeL']
+    }
 
 
 @app.get("/path/{path_id}")
